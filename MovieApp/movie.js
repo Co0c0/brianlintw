142b32adadad156ea0d4fce49cdcb6c8534b0401
Brian.Lin <brian410685028@gmail.com>
2024-02-04 17:34:04 +0800

    [#7] For movie testing

--- MovieApp/movie.js
+++ MovieApp/movie.js
@@ -1,49 +1,57 @@
-const APLINK = 'https://api.themoviedb.org/3/movie/550?api_key=2716caebb598e24dc4385b7cfeb91427'
-const IMG_PATH = 'https://iamge.tmdb.org/t/p/w1280'
-const SEARCHAPI = 'https://api.themovie.org/3/search/movie?api_key=2716caebb598e24dc4385b7cfeb91427&query="'
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2716caebb598e24dc4385b7cfeb91427&page=1';
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=2716caebb598e24dc4385b7cfeb91427&query=";
 
-const main = document.getElementById("section")
-const form = document.getElementById("form")
-const search = document.getElementById("query")
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
 
-returnMovies(APLINK)
+returnMovies(APILINK)
 function returnMovies(url){
     fetch(url).then(res => res.json())
     .then(function(data){
-        console.log(data.result);
-        data.result.forEach(element => {
+        console.log(data.results);
+        data.results.forEach(element => {
             const div_card = document.createElement('div');
-            div_card.setAttribute('class', 'card')
-            const div_row = document.createElement('div');
-            div_card.setAttribute('class', 'row')
-            const div_column = document.createElement('div');
-            div_card.setAttribute('class', 'column')
-            const image = document.createElement('img');
-            div_card.setAttribute('class', 'thumbnail')
-            div_card.setAttribute('id', 'image')
-            const title = document.createElement('h3');
-            div_card.setAttribute('id', 'title')
+            div_card.setAttribute('class', 'card');
             
-            title.innerHTML = `${element.titile}`;
+            const div_row = document.createElement('div');
+            div_row.setAttribute('class', 'row');
+            
+            const div_column = document.createElement('div');
+            div_column.setAttribute('class', 'column');
+            
+            const image = document.createElement('img');
+            image.setAttribute('class', 'thumbnail');
+            image.setAttribute('id', 'image');
+            
+            const title = document.createElement('h3');
+            title.setAttribute('id', 'title');
+            
+            const center = document.createElement('center');
+
+            title.innerHTML = `${element.title}`;
             image.src = IMG_PATH + element.poster_path;
+
+            center.appendChild(image);
+            div_card.appendChild(center);
             div_card.appendChild(title);
             div_column.appendChild(div_card);
             div_row.appendChild(div_column);
 
-            main.appendChild(div_row)
+            main.appendChild(div_row);
         });
     });
-};
+}
 
-form.addEventListener("submit", (e) =>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    main.innerHTML = ''
+    main.innerHTML = '';
 
     const searchItem = search.value;
 
-    if(searchItem){
-        returnMovies(SEARCHAPI + searchItem)
+    if (searchItem) {
+    returnMovies(SEARCHAPI + searchItem);
         search.value = "";
     }
-
 });
